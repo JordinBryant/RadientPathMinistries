3b2ffd6f7be89bd20e45ca52f7b01ae711cbc3da
jordin <jordintaylor095@gmail.com>
2024-09-14 15:38:57 -0400

    added images

--- src/pages/HairCarePage.js
+++ src/pages/HairCarePage.js
@@ -15,9 +15,6 @@ const HairPage = () => {
             <li>
               <a href="#tips">Hair Tips</a>
             </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
           </ul>
         </nav>
       </header>
@@ -43,17 +40,17 @@ const HairPage = () => {
         <h2>Our Hair Care Products</h2>
         <div className="product-list">
           <div className="product">
-            <img src="/images/product1.jpg" alt="Hair Oil" />
+            <img src="/images/hairOil.jpg" alt="Hair Oil" />
             <h3>Organic Hair Oil</h3>
             <p>For silky smooth hair</p>
           </div>
           <div className="product">
-            <img src="/images/product2.jpg" alt="Shampoo" />
+            <img src="/images/shampoo.jpg" alt="Shampoo" />
             <h3>Herbal Shampoo</h3>
             <p>Clean and nourish your scalp</p>
           </div>
           <div className="product">
-            <img src="/images/product3.jpg" alt="Conditioner" />
+            <img src="/images/conditioner.jpg" alt="Conditioner" />
             <h3>Conditioner</h3>
             <p>Hydrate and protect</p>
           </div>
@@ -75,19 +72,21 @@ const HairPage = () => {
             <h4>Tip #2: Wash Less Frequently</h4>
             <p>
               Over-washing can strip your hair of natural oils. Try washing 2-3
-              times a week.
+              times a month.
+            </p>
+          </div>
+          <div className="tip">
+            <h4>Tip #3: Finish Hair washing with cold water</h4>
+            <p>
+              Cold water can generally stimulate the production of white blood
+              cells throughout the body, which are crucial for fighting
+              infections, potentially boosting your overall immune system;
+              therefore, indirectly supporting the immune response in your head
+              as well.
             </p>
           </div>
         </div>
       </section>
-
-      {/* Footer */}
-      <footer className="footer" id="contact">
-        <p>© 2024 Radiant Hair Care. All rights reserved.</p>
-        <p>
-          {/* Follow us on <a href="#">Instagram</a> | <a href="#">Facebook</a> */}
-        </p>
-      </footer>
     </div>
   );
 };
